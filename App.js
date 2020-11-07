8135fd8531474597ef796aeb458f6275ce9b15f7
mpeglerg <mpeglerg@lion.lmu.edu>
2020-11-06 21:09:29 -0800

    connect store and middleware

--- App.js
+++ App.js
@@ -1,8 +1,21 @@
 import HomeScreen from "./src/components/screens/HomeScreen";
 import AccountScreen from "./src/components/screens/AccountScreen";
-import { createAppContainer, createSwitchNavigator } from "react-navigation";
+import {
+  createAppContainer,
+  createSwitchNavigator,
+  StackNavigator,
+} from "react-navigation";
 import { createDrawerNavigator } from "react-navigation-drawer";
 import { createStackNavigator } from "react-navigation-stack";
+import { Provider, connect } from "react-redux";
+import { createStore, applyMiddleware, combineReducers } from "redux";
+import {
+  reduxifyNavigator,
+  createReactNavigationReduxMiddleware,
+  createNavigationReducer,
+  createReduxContainer,
+} from "react-navigation-redux-helpers";
+import React from "react";
 
 const AppDrawer = createDrawerNavigator({
   Home: HomeScreen,
@@ -13,9 +26,38 @@ const AppModalStack = createStackNavigator({
   NiteLite: AppDrawer,
 });
 
-const App = createSwitchNavigator({
+const AppSwitchNavigator = createSwitchNavigator({
   App: {
     screen: AppModalStack,
   },
 });
-export default createAppContainer(App);
+
+const navigationReducer = createNavigationReducer(AppSwitchNavigator);
+
+const appReducer = combineReducers({
+  nav: navigationReducer,
+});
+
+const appMiddleware = createReactNavigationReduxMiddleware(
+  (state) => state.nav
+);
+
+const AppContainer = createReduxContainer(AppSwitchNavigator, "root");
+
+const mapStateToProps = (state) => ({
+  state: state.nav,
+});
+
+const AppWithNavigationState = connect(mapStateToProps)(AppContainer);
+
+const store = createStore(appReducer, applyMiddleware(appMiddleware));
+
+const App = () => {
+  return (
+    <Provider store={store}>
+      <AppWithNavigationState />
+    </Provider>
+  );
+};
+
+export default App;
