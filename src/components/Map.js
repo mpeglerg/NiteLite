30200c520171559978ece58b0c4d360afdeed553
Shanaya Nagendran <shanaya.n@gmail.com>
2020-11-20 15:56:34 -0800

    get walkscore with user current location

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,30 +1,53 @@
-import React, { Component } from "react";
-import { View, StyleSheet } from "react-native";
-import MapView from "react-native-maps";
+import React, { useEffect, useState } from "react";
+import { View, StyleSheet, Text, Alert } from "react-native";
+import MapView, { Marker, Callout } from "react-native-maps";
 import MapViewDirections from "react-native-maps-directions";
 import { connect } from "react-redux";
+import { getScore } from "../../data/walkScoreApi";
 
 const GOOGLE_MAPS_APIKEY = process.env.REACT_APP_GOOGLE_MAPS_API;
 
 const styles = StyleSheet.create({
-    versionBox: {
-      position: 'absolute',
-      bottom: 50,
-      right: 50,
-      flexDirection: 'row',
-      justifyContent: 'flex-end',
-    },
-    versionText: {
-      padding: 4,
-      backgroundColor: '#FFF',
-      color: '#000',
+    walkScoreView: {
+      flex: 1,
+      width: 150,
+      height: 40,
+      backgroundColor: 'rgba(255,255,255,0.7)',
+      alignItems: 'center',
     },
   });
 
   const MapContainer = (props) => {
     const [coordinates, setCoordinates] = useState([]);
     const [mapUIView, setMapUIView] = useState(null);
+    const [list, setList] = useState([]);
     
+    let currLoc = {
+      latitude: 37.771707,
+      longitude: -122.4053769
+    }
+
+    useEffect(() => {
+      findCoordinates()
+      let mounted = true;
+      getScore(currLoc).then(items => {
+        if(mounted) {
+          setList(items)
+        }
+      })
+      return() => mounted = false;
+    }, [])
+
+    const findCoordinates = () => {
+      navigator.geolocation.getCurrentPosition(
+        position => {
+          currLoc = {longitude: position.coords.longitude, latitude: position.coords.latitude}
+        },
+        error => Alert.alert(error.message),
+        { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
+      )
+    }
+
     const onMapPress = (e) => {
       setCoordinates([
         ...coordinates,
@@ -48,25 +71,46 @@ const styles = StyleSheet.create({
     };
   
       return(
-          <View style={StyleSheet.absoluteFill}>
-              <MapView initialRegion={{
-                  latitude: 37.771707,
-                  longitude: -122.4053769,
-                  latitudeDelta: 0.0922,
-                  longitudeDelta: 0.0922,
+        <View style={StyleSheet.absoluteFill}>
+          <MapView initialRegion={{
+                latitude: 37.771707,
+                longitude: -122.4053769,
+                latitudeDelta: 0.0922,
+                longitudeDelta: 0.0922,
               }}
               style={StyleSheet.absoluteFill}
               ref={c => setMapUIView(c)} // eslint-disable-line react/jsx-no-bind
               onPress={onMapPress}>
-              <MapViewDirections
-                  origin={props.directions.directions[0].origin}
-                  destination={props.directions.directions[0].destination}
-                  apikey={GOOGLE_MAPS_APIKEY}
-                  strokeWidth={3}
-                  strokeColor="hotpink"
-              />
-              </MapView>
-          </View>
+            <MapViewDirections
+                origin={props.directions.directions[0].origin}
+                destination={props.directions.directions[0].destination}
+                apikey={GOOGLE_MAPS_APIKEY}
+                strokeWidth={3}
+                strokeColor="hotpink"
+            />
+            <Marker coordinate={currLoc}>
+              <Callout
+                // alphaHitTest
+                // tooltip
+                // onPress={e => {
+                //   if (
+                //     e.nativeEvent.action === 'marker-inside-overlay-press' ||
+                //     e.nativeEvent.action === 'callout-inside-press'
+                //   ) {
+                //     return;
+                //   }
+
+                //   Alert.alert('callout pressed');
+                // }}
+                style={styles.walkScoreView}
+              >
+                <View>
+                  <Text>{`WalkScore: ${list.walkscore}\n${list.description}`}</Text>
+                </View>
+              </Callout>
+            </Marker>
+          </MapView>
+        </View>
       );
 };
 
