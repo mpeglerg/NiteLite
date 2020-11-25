1590733f7d250f9e4bf7e09f6232a05506615b0f
Shanaya Nagendran <shanaya.n@gmail.com>
2020-11-24 19:00:51 -0800

    reset overwrite

--- src/reducers/DirectionsReducer.js
+++ src/reducers/DirectionsReducer.js
@@ -1,22 +1,29 @@
 const initState = {
-    directions: [
-        {origin: '',
-        destination: ''}
-    ]
-  };
-  
-  const directionsReducer = (state = initState, action) => {
-    if (action.type == "UPDATE_DIRECTIONS") {
-  
-      let newDirections = state.directions;
-      newDirections[0].destination = action.payload;
-      return {
-        ...state,
-        directions: newDirections,
-      };
-    } else {
-      return state;
-    }
-  };
-  
-  export default directionsReducer;
+  directions: [
+    {
+      origin: "Twitter HQ, Market Street, San Francisco, CA, USA",
+      destination: "Apple Park Visitor Center",
+    },
+  ],
+  route: [],
+};
+
+const directionsReducer = (state = initState, action) => {
+  if (action.type == "UPDATE_DIRECTIONS") {
+    let newDirections = state.directions;
+    newDirections[0].destination = action.payload;
+    return {
+      ...state,
+      directions: newDirections,
+    };
+  } else if (action.type == "UPDATE_CURRENT_ROUTE") {
+    let newRoute = action.payload;
+    return {
+      ...state,
+      route: newRoute,
+    };
+  } else {
+    return state;
+  }
+};
+export default directionsReducer;
