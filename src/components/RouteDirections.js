e8a4854573f530079377168e73851a3ac2eeb59b
Talia <taliabahar@yahoo.com>
2020-11-26 00:02:38 -0800

    uncommented RouteDirections.js

--- src/components/RouteDirections.js
+++ src/components/RouteDirections.js
@@ -1,39 +1,39 @@
-// import React from "react";
-// import { Text, View } from "react-native";
-// import { connect } from "react-redux";
+import React from "react";
+import { Text, View } from "react-native";
+import { connect } from "react-redux";
 
-// const RouteDirections = (props) => {
-//   return (
-//     <View>
-//       {/* {props.route.route[0].routes[0].legs.map((routeLeg, index) => { */}
-//         return (
-//           <View>
-//             <Text>Route Directions</Text>
-//             {/* <Text>{routeLeg.distance.text}</Text> */}
-//             {/* {console.log("routeLeg.distance.text: ", routeLeg.distance.text)} */}
-//             {/* <Text>{routeLeg.duration.text}</Text> */}
-//           </View>
-//         );
-//       {/* })} */}
-//       {/* {props.route.route[0].routes[0].legs[0].steps.map((step, index) => { */}
-//         return (
-//           <View>
-//             {/* <Text>Step #{index}</Text>
+const RouteDirections = (props) => {
+  return (
+    <View>
+      {props.route.route[0].routes[0].legs.map((routeLeg, index) => {
+        return (
+          <View>
+            <Text>Route Directions</Text>
+            <Text>{routeLeg.distance.text}</Text>
+            {console.log("routeLeg.distance.text: ", routeLeg.distance.text)}
+            <Text>{routeLeg.duration.text}</Text>
+          </View>
+        );
+      })} 
+      {props.route.route[0].routes[0].legs[0].steps.map((step, index) => {
+        return (
+          <View>
+            <Text>Step #{index}</Text>
 
-//             <Text>{step.distance.text}</Text>
-//             <Text>{step.duration.text}</Text>
-//             <Text>
-//               {step.html_instructions ? step.html_instructions : null}
-//             </Text> */}
-//           </View>
-//         );
-//       {/* })} */}
-//     </View>
-//   );
-// };
-// const mapStateToProps = (state) => {
-//   return {
-//     route: state.directions,
-//   };
-// };
-// export default connect(mapStateToProps, null)(RouteDirections);
+            <Text>{step.distance.text}</Text>
+            <Text>{step.duration.text}</Text>
+            <Text>
+              {step.html_instructions ? step.html_instructions : null}
+            </Text>
+          </View>
+        );
+     })}
+    </View>
+  );
+};
+const mapStateToProps = (state) => {
+  return {
+    route: state.directions,
+  };
+};
+export default connect(mapStateToProps, null)(RouteDirections);
