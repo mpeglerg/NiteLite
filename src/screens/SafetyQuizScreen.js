b5d58d5ecf66820a88206da0b30eacdb6e5f02a7
Lauren Lindsey <llindse3@lion.lmu.edu>
2020-12-08 22:35:02 -0800

    get checkboxes to register

--- src/screens/SafetyQuizScreen.js
+++ src/screens/SafetyQuizScreen.js
@@ -1,14 +1,13 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
 import { TextInput } from "react-native-gesture-handler";
-import CheckBox from "../components/CheckBox";
-// import { CheckBox } from '@react-native-community/checkbox';
-import AddContactIcon from 'react-native-vector-icons/AntDesign';
-import AddContactIcon2 from 'react-native-vector-icons/MaterialIcons';
-import {colors} from "../styles/colors.js"
+import Icon from "react-native-vector-icons/MaterialIcons";
+import AddContactIcon from "react-native-vector-icons/MaterialIcons";
+import { colors } from "../styles/colors.js";
 
-const SafetyQuizScreen = ({navigation}) => {
-  let object = navigation.getParam('object','missing');
+const SafetyQuizScreen = ({ navigation }) => {
+  let object = navigation.getParam("object", "missing");
   const [safePlaceInput, setSafePlaceInput] = useState("");
   const [openBusinesses, setOpenBusinesses] = useState(false);
   const [policeStations, setPoliceStations] = useState(false);
@@ -18,31 +17,62 @@ const SafetyQuizScreen = ({navigation}) => {
     <View style={styles.container}>
       {/* <Text>Account</Text> */}
       <Text style={styles.header}>Set Up Your Route Preferences</Text>
-      
-      <Text style={styles.taskText}>1. What makes you feel safe when walking?</Text>
+
+      <Text style={styles.taskText}>
+        1. What makes you feel safe when walking?
+      </Text>
       <View style={styles.check}>
-        <CheckBox 
-        value={openBusinesses}
-        onValueChange={(isSelected)=> {alert("toggled!"); setOpenBusinesses(isSelected)}}
-        />
+        <TouchableOpacity
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+          }}
+          onPress={() => setOpenBusinesses(!openBusinesses)}
+        >
+          <Icon
+            size={30}
+            color={"#FFFFFF"}
+            name={openBusinesses ? "check-box" : "check-box-outline-blank"}
+          />
+        </TouchableOpacity>
         <Text style={styles.checkOptions}>Open Businesses</Text>
       </View>
       <View style={styles.check}>
-        <CheckBox
-        value={policeStations}
-        onValueChange={setPoliceStations}
-        />
+        <TouchableOpacity
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+          }}
+          onPress={() => setPoliceStations(!policeStations)}
+        >
+          <Icon
+            size={30}
+            color={"#FFFFFF"}
+            name={policeStations ? "check-box" : "check-box-outline-blank"}
+          />
+        </TouchableOpacity>
         <Text style={styles.checkOptions}>Police Stations</Text>
       </View>
       <View style={styles.check}>
-        <CheckBox
-        value={busySidewalks}
-        onValueChange={setBusySidewalks}
-        />
+        <TouchableOpacity
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+          }}
+          onPress={() => setBusySidewalks(!busySidewalks)}
+        >
+          <Icon
+            size={30}
+            color={"#FFFFFF"}
+            name={busySidewalks ? "check-box" : "check-box-outline-blank"}
+          />
+        </TouchableOpacity>
         <Text style={styles.checkOptions}>Busy Sidewalks</Text>
       </View>
-      
-      <Text style={styles.taskText}>2. Enter the addresses of places you consider "Safe Spots"</Text>
+
+      <Text style={styles.taskText}>
+        2. Enter the addresses of places you consider "Safe Spots"
+      </Text>
       <TextInput
         style={{
           height: 40,
@@ -58,40 +88,43 @@ const SafetyQuizScreen = ({navigation}) => {
         value={safePlaceInput}
       />
 
-     
-        <Text style={styles.taskText}>3. Set up Emergency Contacts</Text>
-        {/* <View> */}
-        <TouchableOpacity onPress={ () => 
-        {objectifyAndNav(navigation, object, busySidewalks, openBusinesses, policeStations, safePlaceInput);}}>
-        <AddContactIcon2 size={38} name="person-add" color="white"/>
+      <Text style={styles.taskText}>3. Set up Emergency Contacts</Text>
+      {/* <View> */}
+      <TouchableOpacity
+        onPress={() => {
+          objectifyAndNav(
+            navigation,
+            object,
+            openBusinesses,
+            policeStations,
+            busySidewalks,
+            safePlaceInput
+          );
+        }}
+      >
+        <AddContactIcon size={38} name="person-add" color="white" />
         {/* </View> */}
       </TouchableOpacity>
     </View>
   );
 };
 
-function objectifyAndNav(navigation, object, busySidewalks, openBusinesses, policeStations, safePlaceInput){
+function objectifyAndNav(
+  navigation,
+  object,
+  openBusinesses,
+  policeStations,
+  busySidewalks,
+  safePlaceInput
+) {
   // add new items to our object
-  console.log("busy sidewalks " + busySidewalks)
-  console.log("openBusinesses " + openBusinesses)
-  console.log("policeStations " + policeStations)
-
-
-  // if(busySidewalks == false) { busySidewalks = !busySidewalks; }
-  // if(openBusinesses == false) { openBusinesses = !openBusinesses; }
-  // if(policeStations == false) { policeStations = !policeStations; }
-  // if(busySidewalks == "") { busySidewalks = false; }
-  // if(openBusinesses == "") { busySidewalks = false; }
-  // if(policeStations == "") { busySidewalks = false; }
-
   object.set("busySidewalks", busySidewalks);
   object.set("openBusinesses", openBusinesses);
   object.set("policeStations", policeStations);
   object.set("safePlaces", safePlaceInput);
 
   // navigate to next page
-  navigation.navigate('EmergencyContacts', { object: object });
-
+  navigation.navigate("EmergencyContacts", { object: object });
 }
 
 export default SafetyQuizScreen;
@@ -116,14 +149,14 @@ const styles = StyleSheet.create({
 
     textShadowOffset: { width: 2, height: 2 },
     textShadowRadius: 8,
-    textShadowColor: '#F2EB46',
+    textShadowColor: "#F2EB46",
   },
   check: {
     display: "flex",
     flexDirection: "row",
-    alignItems: 'center',
+    alignItems: "center",
     padding: 10,
-    color: "#fff"
+    color: "#fff",
     // margin: 8
   },
   checkOptions: {
