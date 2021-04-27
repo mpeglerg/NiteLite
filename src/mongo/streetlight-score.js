27d8fc69e1366fc2b43c99370bb23e297c4a3fb9
Shanaya Nagendran <shanaya.n@gmail.com>
2021-04-27 14:41:20 -0700

    wip connect mongo

--- src/mongo/streetlight-score.js
+++ src/mongo/streetlight-score.js
@@ -1,16 +1,21 @@
 // Example mongo query for all crimes within a certain radius
 // import { REACT_APP_MONGO_URI } from 'react-native-dotenv'
 
-// const getStreetlightScore = (origin, destination) => {
+const getStreetlightScore = (origin, destination) => {
     const MongoClient = require('mongodb').MongoClient
     const uri = "mongodb+srv://snagendran:EOcJhkCgpeJP3Z0h@cluster0.eo6ns.mongodb.net/NiteLite?retryWrites=true&w=majority"
     const client = new MongoClient(uri, { useUnifiedTopology: true, useNewUrlParser: true })
+
+    console.log("ORIGIN: ", origin)
+    console.log("DESTINATION: ", destination)
+
     client.connect(async (err) => {
         const collection = client.db("NiteLite").collection("streetlights")
         const radius = 5000
-        const origin = {lng: -118.4179, lat: 33.9697}
-        const destination = {lng: -118.4180, lat: 33.9698}
+        // const origin = {lng: -118.4179, lat: 33.9697}
+        // const destination = {lng: -118.4180, lat: 33.9698}
         const query = {
+            // "location.coordinates": { $gt: -118.424, $lt: -118.418422 }
             $and: [
                 {"location.coordinates": { $gt: origin.lng, $lt: destination.lng }},
                 {"location.coordinates": { $gt: origin.lat, $lt: destination.lat }}
@@ -18,14 +23,17 @@
         }
         const options = {
             sort: { location: 1, },
+            // limit: 10
         }
+
         const numStreetlights = await collection.countDocuments(query, options)
-        
         console.log(numStreetlights)
+        // const numStreetlights = await collection.find(query, options)
+        // await numStreetlights.forEach(console.log)
         // return numStreetlights
 
         client.close()
     });
-// }
+}
 
-// export default {getStreetlightScore};
+export default getStreetlightScore;
