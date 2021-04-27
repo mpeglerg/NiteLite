10bd9695b10f8eb6258b3201522acd92917bdbe5
Shanaya Nagendran <shanaya.n@gmail.com>
2021-04-26 18:01:27 -0700

    wip along route scores

--- src/mongo/crime-score.js
+++ src/mongo/crime-score.js
@@ -1,13 +1,10 @@
-// Example mongo query for all crimes within a certain radius
-// import { REACT_APP_MONGO_URI } from 'react-native-dotenv'
-
-// const getStreetlightScore = (origin, destination) => {
+// const getCrimeScore = (origin, destination) => {
     const MongoClient = require('mongodb').MongoClient
     const uri = "mongodb+srv://snagendran:EOcJhkCgpeJP3Z0h@cluster0.eo6ns.mongodb.net/NiteLite?retryWrites=true&w=majority"
     const client = new MongoClient(uri, { useUnifiedTopology: true, useNewUrlParser: true })
     client.connect(async (err) => {
-        const collection = client.db("NiteLite").collection("streetlights")
-        const radius = 5000
+        const collection = client.db("NiteLite").collection("crimes")
+        // [lng,lat]
         const origin = {lng: -118.4179, lat: 33.9697}
         const destination = {lng: -118.4180, lat: 33.9698}
         const query = {
@@ -19,13 +16,13 @@
         const options = {
             sort: { location: 1, },
         }
-        const numStreetlights = await collection.countDocuments(query, options)
-        
-        console.log(numStreetlights)
-        // return numStreetlights
+        const numCrimes = await collection.countDocuments(query, options)
+
+        console.log(numCrimes)
+        // return numCrimes
 
         client.close()
     });
 // }
 
-// export default {getStreetlightScore};
+// export default {getCrimeScore};
